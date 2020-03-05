480f13223a100b7559a82b08ce266459d7d3ac0a
aluno <aluno@Lab-23>
2020-03-05 10:21:05 -0300

    trabalho 3

---
+++ script-for.sh
@@ -0,0 +1,14 @@
+#meuscriptconvertimagens
+#testescript
+#convençãodeimagens
+#autor: Leticia Leandro Felix
+#!/bin/bash
+
+cd imagens-livros
+for imagem in *.jpg
+do
+	echo $imagem
+	img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
+	convert $imagem  $img_sem_ext.png
+done
+cd ..
